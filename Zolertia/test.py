ec7553a8ec67f1e30686314faa40864fc73ee079
ELDiablO59152 <fabienamelinck@hotmail.fr>
2022-04-19 02:49:27 +0200

    UseInit

--- Zolertia/test.py
+++ Zolertia/test.py
@@ -2,6 +2,15 @@ import subprocess # for lora transmit C code
 from threading import Thread # for lora receive thread
 from time import sleep
 
+if False:
+    proc = subprocess.Popen(["../Rasp/Init"], shell=False, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    print(proc) # Ordonne au raspberry d'allumer la LED
+
+    stdout, stderr = proc.communicate(timeout=15)
+    print("Output:\n", stdout.decode('utf-8'), stderr.decode('utf-8'))
+
+sleep(5)
+
 if True:
     proc = subprocess.Popen(["../Rasp/Transmit", "LED_ON"], shell=False, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     print(proc) # Ordonne au raspberry d'allumer la LED
