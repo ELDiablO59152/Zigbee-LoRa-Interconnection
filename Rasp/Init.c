136b2b4208a8e5c1bf111b301b9356c7913b3354
ELDiablO59152 <fabienamelinck@hotmail.fr>
2022-04-21 15:17:13 +0200

    Print args during init

--- Rasp/Init.c
+++ Rasp/Init.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]) {
     for (int i; i < argc; i++) {
         fprintf(stdout, " %s", argv[i]);
     }
+    fprintf(stdout, "\n");
 
     if (init_spi()) return -1;
 
